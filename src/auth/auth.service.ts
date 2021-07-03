1e2ba1f512cb77186d0280bf92fdb3653af3ce0f
5linesys <dhtjstlr777@gmail.com>
2021-07-03 22:54:29 +0900

    Add auth provider

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { USER } from '@prisma/client';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, pass: string): Promise<USER> {
+    const user = await this.usersService.getUser({ email: email });
+    if (user && user.pass === pass) return user;
+    return null;
+  }
+
+  async getToken(user: any) {
+    const payload = { email: user.email, sub: user.id };
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { pass, ...result } = user;
+    return {
+      access_token: this.jwtService.sign(payload),
+      ...result,
+    };
+  }
+}
