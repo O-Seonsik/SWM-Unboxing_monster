4076d633e356eb14623f1579a344fbe79c7ea1f1
5linesys <dhtjstlr777@gmail.com>
2021-07-21 16:20:52 +0900

    Modify Structure without provider, Add exception filter in createBox method

--- src/box/box.module.ts
+++ src/box/box.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BoxController } from './box.controller';
-import { BoxService } from './box.service';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [BoxController],
-  providers: [BoxService, PrismaService],
+  providers: [PrismaService],
 })
 export class BoxModule {}
