ea3b01a80050dfd17423813ba41d9ff2032c89ec
5linesys <dhtjstlr777@gmail.com>
2021-07-21 15:42:05 +0900

    Finish item CRUD

--- src/item/dto/update-item.dto.ts
+++ src/item/dto/update-item.dto.ts
@@ -1,21 +1,34 @@
-// import { PartialType } from '@nestjs/mapped-types';
-// import { CreateItemDto } from './create-item.dto';
-// import { IsEmail, IsNumber, IsOptional } from 'class-validator';
-//
-// export class UpdateItemDto extends PartialType(CreateItemDto) {
-//   readonly email
-//
-//   /**
-//    * User's email
-//    * @example "email@mail.com"
-//    */
-//   @IsEmail()
-//   readonly email: string;
-//   /**
-//    * Points that users have
-//    * @example 5000
-//    */
-//   @IsNumber()
-//   @IsOptional()
-//   readonly point?: number;
-// }
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateItemDto } from './create-item.dto';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class UpdateItemDto extends PartialType(CreateItemDto) {
+  /**
+   * 상품명
+   * @example '최고급 한우'
+   */
+  @IsOptional()
+  @IsString()
+  readonly title?: string;
+  /**
+   * 상품가격
+   * @example '20000'
+   */
+  @IsOptional()
+  @IsNumber()
+  readonly price?: number;
+  /**
+   * 상품 대표 이미지 url
+   * @example 'https://test.com/resource/test.png
+   */
+  @IsOptional()
+  @IsString()
+  readonly image?: string;
+  /**
+   * 상품 세부 설명
+   * @example '최고급 한우입니다. 풀을 많이 먹고 자라서 그런지 풀맛이 나요'
+   */
+  @IsOptional()
+  @IsString()
+  readonly detail?: string;
+}
--- src/item/item.controller.ts
+++ src/item/item.controller.ts
@@ -1,32 +1,74 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
-import { ItemService } from './item.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  ForbiddenException,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateItemDto } from './dto/create-item.dto';
+import { UpdateItemDto } from './dto/update-item.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { Item } from '@prisma/client';
 
+@ApiTags('Items')
 @Controller('item')
 export class ItemController {
-  constructor(private readonly itemService: ItemService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
   @Get()
-  async getItems() {
-    return this.itemService.getItems();
+  async getItems(): Promise<Item[]> {
+    return await this.prismaService.item.findMany();
   }
 
   @Get(':id')
-  async getItem(@Param('id') id: number) {
-    return this.itemService.getItem({ id: id });
+  async getItem(@Param('id') id: number): Promise<Item> {
+    return await this.prismaService.item.findUnique({ where: { id: id } });
   }
 
   @Post()
-  async createItem(@Body() box) {
-    return this.itemService.createItem(box);
+  async createItem(@Body() box: CreateItemDto): Promise<Item> {
+    return await this.prismaService.item.create({ data: box });
   }
 
   @Patch(':id')
-  async updateItem() {
-
+  async updateItem(
+    @Body() body: UpdateItemDto,
+    @Param('id') id: number,
+  ): Promise<Item> {
+    try {
+      return await this.prismaService.item.update({
+        where: { id: id },
+        data: {
+          title: body.title,
+          price: body.price,
+          image: body.image,
+          detail: body.detail,
+        },
+      });
+    } catch (error) {
+      if (error.code === 'P2025')
+        throw new NotFoundException(error.code, error.meta.cause);
+      if (error.code === 'P2002')
+        throw new ForbiddenException(error.code, error.meta.target);
+      return error;
+    }
   }
 
   @Delete(':id')
-  async deleteItem() {
-
+  async deleteItem(@Param('id') id: number): Promise<Item> {
+    try {
+      return await this.prismaService.item.delete({ where: { id: id } });
+    } catch (error) {
+      if (error.code === 'P2025')
+        throw new NotFoundException(error.code, error.meta.cause);
+      if (error.code === 'P2002')
+        throw new ForbiddenException(error.code, error.meta.target);
+      return error;
+    }
   }
 }
