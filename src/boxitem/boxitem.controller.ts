0b06188968886f725ae76e7caa9a9edb9425841d
5linesys <dhtjstlr777@gmail.com>
2021-07-22 12:41:42 +0900

    Modify boxItem structure

--- src/boxitem/boxitem.controller.ts
+++ src/boxitem/boxitem.controller.ts
@@ -1,21 +1,19 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { BoxItemService } from './boxitem.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Controller('boxitem')
 export class BoxItemController {
-  constructor(private readonly boxItemService: BoxItemService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
   @Get(':id')
-  async getBoxItem(@Param('id') box) {
+  async getBoxItem(@Param('id') id: number) {
     // box_id 형변환 transfomer 에게 위임하기
     // BoxItem 을 활용해서 Box의 Item 리스트 확인해보기
-    const boxId = { boxId: +box };
-    return this.boxItemService.getItems(boxId);
+    return this.prismaService.boxItem.findUnique({ where: { id: id } });
   }
 
   @Post()
   async createBoxItems(@Body('data') box) {
-    console.log(box);
-    return this.boxItemService.createBoxItems(box);
+    return this.prismaService.boxItem.create(box);
   }
 }
