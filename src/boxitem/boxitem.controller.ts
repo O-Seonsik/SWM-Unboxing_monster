fb902a5efebd75b690b3c816dc973b4778180f6e
5linesys <dhtjstlr777@gmail.com>
2021-07-22 15:49:13 +0900

    Modify createBoxItems method

--- src/boxitem/boxitem.controller.ts
+++ src/boxitem/boxitem.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateBoxItemDto } from './dto/create-box-item.dto';
 
 @Controller('boxitem')
 export class BoxItemController {
@@ -9,11 +10,11 @@ export class BoxItemController {
   async getBoxItem(@Param('id') id: number) {
     // box_id 형변환 transfomer 에게 위임하기
     // BoxItem 을 활용해서 Box의 Item 리스트 확인해보기
-    return this.prismaService.boxItem.findUnique({ where: { id: id } });
+    return await this.prismaService.boxItem.findUnique({ where: { id: id } });
   }
 
   @Post()
-  async createBoxItems(@Body('data') box) {
-    return this.prismaService.boxItem.create(box);
+  async createBoxItems(@Body() box: CreateBoxItemDto) {
+    return await this.prismaService.boxItem.createMany(box);
   }
 }
