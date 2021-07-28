17bed06f74006f8e7cc682d6f7380404b9044c94
5linesys <dhtjstlr777@gmail.com>
2021-07-28 13:55:19 +0900

    Import provider of notice.service.ts

--- src/notice/notice.module.ts
+++ src/notice/notice.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NoticeController } from './notice.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { NoticeService } from './notice.service';
 
 @Module({
   controllers: [NoticeController],
-  providers: [PrismaService],
+  providers: [PrismaService, NoticeService],
 })
 export class NoticeModule {}
