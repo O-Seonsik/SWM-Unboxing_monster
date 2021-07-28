3ee1d0dfdd283f6698be03bc3a004e91fda15cd9
5linesys <dhtjstlr777@gmail.com>
2021-07-28 23:35:08 +0900

    Create refundable purchase through some methods(createPurchase, refundPurchase)

--- src/purchase/purchase.controller.ts
+++ src/purchase/purchase.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { PurchaseService } from './purchase.service';
+import { Purchase } from '@prisma/client';
+import { ApiTags } from '@nestjs/swagger';
+import { CreatePurchaseDto } from './dto/create-purchase.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
+@ApiTags('purchase')
 @Controller('purchase')
-export class PurchaseController {}
+export class PurchaseController {
+  constructor(
+    private readonly purchaseService: PurchaseService,
+    private readonly prismaService: PrismaService,
+  ) {}
+
+  @Get()
+  async getPurchases(): Promise<Purchase[]> {
+    return await this.purchaseService.getPurchases();
+  }
+
+  @Get(':id')
+  async getPurchase(@Param('id') id: number): Promise<Purchase> {
+    return await this.purchaseService.getPurchase({ id: id });
+  }
+
+  @Post()
+  async createPurchase(@Body() body: CreatePurchaseDto) {
+    return await this.purchaseService.createPurchase(body);
+  }
+
+  @Patch(':id')
+  async refundPurchase(@Param('id') id: number) {
+    return await this.purchaseService.refundPurchase({ id: id });
+  }
+}
--- src/purchase/purchase.service.ts
+++ src/purchase/purchase.service.ts
@@ -1,4 +1,97 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Prisma, Purchase } from '@prisma/client';
+import { CreatePurchaseDto } from './dto/create-purchase.dto';
+import { timestamp } from 'rxjs/operators';
 
 @Injectable()
-export class PurchaseService {}
+export class PurchaseService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getPurchases(): Promise<Purchase[]> {
+    return await this.prismaService.purchase.findMany();
+  }
+
+  async getPurchase(
+    purchaseWhereUniqueInput: Prisma.PurchaseWhereUniqueInput,
+  ): Promise<Purchase> {
+    return await this.prismaService.purchase.findUnique({
+      where: purchaseWhereUniqueInput,
+      include: { owner: true, boxes: { include: { box: true } } },
+    });
+  }
+
+  async createPurchase(body: CreatePurchaseDto): Promise<Purchase> {
+    const { ownerId, price, boxesId } = body;
+    const purchase = await this.prismaService.purchase.create({
+      data: {
+        ownerId: ownerId,
+        price: price,
+        refundAt: null,
+      },
+    });
+
+    const boxPurchase = await boxesId.map((boxId) => {
+      return { boxId: boxId, purchaseId: purchase.id };
+    });
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    await this.createBoxPurchase(boxPurchase);
+
+    return await this.prismaService.purchase.findUnique({
+      where: { id: purchase.id },
+      include: { owner: true },
+    });
+  }
+
+  async createBoxPurchase(
+    boxPurchaseCreateManyInput: Prisma.BoxPurchaseCreateManyInput,
+  ): Promise<boolean> {
+    try {
+      await this.prismaService.boxPurchase.createMany({
+        data: boxPurchaseCreateManyInput,
+      });
+      return true;
+    } catch (error) {
+      if (error.code === 'P2003')
+        throw new NotFoundException(
+          `The ${error.meta.field_name} doesn't exist in our service`,
+        );
+      console.log(error);
+      return error;
+    }
+  }
+
+  async refundPurchase(
+    purchaseWhereUniqueInput: Prisma.PurchaseWhereUniqueInput,
+  ): Promise<Purchase> {
+    const purchase = await this.prismaService.purchase.findUnique({
+      where: purchaseWhereUniqueInput,
+    });
+    if (purchase.refundAt)
+      throw new ForbiddenException(
+        'Forbidden Error',
+        'Already refund this purchase.',
+      );
+    try {
+      return await this.prismaService.purchase.update({
+        where: purchaseWhereUniqueInput,
+        data: {
+          refund: true,
+          refundAt: new Date(),
+        },
+      });
+    } catch (error) {
+      if (error.code === 'P2025')
+        throw new NotFoundException(error.code, error.meta.cause);
+      if (error.code === 'P2002')
+        throw new ForbiddenException(error.code, error.meta.cause);
+      return error;
+    }
+  }
+}
