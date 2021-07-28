1dc0d896826d5390548bfa216722d7809cc9f4a1
5linesys <dhtjstlr777@gmail.com>
2021-07-28 13:55:49 +0900

    Modify notice.controller.ts cause create notice.service.ts

--- src/notice/notice.controller.ts
+++ src/notice/notice.controller.ts
@@ -2,37 +2,35 @@ import {
   Body,
   Controller,
   Delete,
-  ForbiddenException,
   Get,
-  NotFoundException,
   Param,
   Patch,
   Post,
 } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
 import { UpdateNoticeDto } from './dto/update-notice.dto';
 import { CreateNoticeDto } from './dto/create-notice.dto';
 import { Notice } from '@prisma/client';
 import { ApiTags } from '@nestjs/swagger';
+import { NoticeService } from './notice.service';
 
 @ApiTags('notice')
 @Controller('notice')
 export class NoticeController {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly noticeService: NoticeService) {}
 
   @Get()
   async getNotices(): Promise<Notice[]> {
-    return this.prismaService.notice.findMany();
+    return await this.noticeService.getNotices();
   }
 
   @Get(':id')
   async getNotice(@Param('id') id: number): Promise<Notice> {
-    return this.prismaService.notice.findUnique({ where: { id: id } });
+    return this.noticeService.getNotice({ id: id });
   }
 
   @Post()
   async createNotice(@Body() notice: CreateNoticeDto): Promise<Notice> {
-    return this.prismaService.notice.create({ data: notice });
+    return await this.noticeService.createNotice(notice);
   }
 
   @Patch(':id')
@@ -40,33 +38,14 @@ export class NoticeController {
     @Body() body: UpdateNoticeDto,
     @Param('id') id: number,
   ): Promise<Notice> {
-    try {
-      return await this.prismaService.notice.update({
-        where: { id: id },
-        data: {
-          imgUrl: body.imgUrl,
-          srcUrl: body.srcUrl,
-        },
-      });
-    } catch (error) {
-      if (error.code === 'P2025')
-        throw new NotFoundException(error.code, error.meta.cause);
-      if (error.code === 'P2002')
-        throw new ForbiddenException(error.code, error.meta.target);
-      return error;
-    }
+    return await this.noticeService.updateNotice({
+      where: { id: id },
+      data: body,
+    });
   }
 
   @Delete(':id')
   async deleteNotice(@Param('id') id: number): Promise<Notice> {
-    try {
-      return await this.prismaService.notice.delete({ where: { id: id } });
-    } catch (error) {
-      if (error.code === 'P2025')
-        throw new NotFoundException(error.code, error.meta.cause);
-      if (error.code === 'P2002')
-        throw new ForbiddenException(error.code, error.meta.target);
-      return error;
-    }
+    return await this.noticeService.deleteNotice({ id: id });
   }
 }
