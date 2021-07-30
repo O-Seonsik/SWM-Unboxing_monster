a47fb9ff2aa84c276e2d70e2d9c2b41d070e70ca
5linesys <dhtjstlr777@gmail.com>
2021-07-30 17:39:43 +0900

    Add some methods in coupon module for user store

--- src/coupon/coupon.controller.ts
+++ src/coupon/coupon.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CouponService } from './coupon.service';
+import { Coupon } from '@prisma/client';
+import { CreateCouponDto } from './dto/create-coupon.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('coupon')
 @Controller('coupon')
 export class CouponController {
   constructor(private readonly couponService: CouponService) {}
+
+  @Get()
+  async getCoupons(): Promise<Coupon[]> {
+    return await this.couponService.getCoupons();
+  }
+
+  @Get(':id')
+  async getUserCoupon(@Param('id') id: string): Promise<Coupon[]> {
+    return await this.couponService.getUserCoupon({ ownerId: id });
+  }
+
+  @Post()
+  async createCoupon(@Body() body: CreateCouponDto): Promise<Coupon> {
+    return await this.couponService.createCoupon(body);
+  }
+
+  @Delete(':id')
+  async deleteCoupon(@Param('id') id: number): Promise<Coupon> {
+    return await this.couponService.deleteCoupon({ id: id });
+  }
 }
--- src/coupon/coupon.service.ts
+++ src/coupon/coupon.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
-
+import { PrismaService } from '../prisma/prisma.service';
+import { Prisma, Coupon } from '@prisma/client';
+import { CreateCouponDto } from './dto/create-coupon.dto';
 @Injectable()
-export class CouponService {}
+export class CouponService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getCoupons(): Promise<Coupon[]> {
+    return await this.prismaService.coupon.findMany({
+      include: { owner: true },
+    });
+  }
+
+  async getUserCoupon(
+    couponWhereInput: Prisma.CouponWhereInput,
+  ): Promise<Coupon[]> {
+    return await this.prismaService.coupon.findMany({
+      where: couponWhereInput,
+      include: {
+        owner: true,
+        item: true,
+      },
+    });
+  }
+
+  async createCoupon(body: CreateCouponDto): Promise<Coupon> {
+    const { ownerId, itemId, qr } = body;
+    return await this.prismaService.coupon.create({
+      data: {
+        ownerId: ownerId,
+        itemId: itemId,
+        qr: qr,
+        isUse: false,
+      },
+    });
+  }
+
+  async deleteCoupon(
+    couponWhereUniqueInput: Prisma.CouponWhereUniqueInput,
+  ): Promise<Coupon> {
+    return await this.prismaService.coupon.delete({
+      where: couponWhereUniqueInput,
+    });
+  }
+}
