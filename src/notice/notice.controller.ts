ef29ab493fd35ab5cd264dbc89ebb3d2cab2b9ac
5linesys <dhtjstlr777@gmail.com>
2021-09-20 03:09:38 +0900

    Add notice ApiOperation decorator

--- src/notice/notice.controller.ts
+++ src/notice/notice.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { Notice } from '@prisma/client';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { NoticeService } from './notice.service';
 
 @ApiTags('notice')
@@ -8,11 +8,13 @@ import { NoticeService } from './notice.service';
 export class NoticeController {
   constructor(private readonly noticeService: NoticeService) {}
 
+  @ApiOperation({ summary: '모든 게시글 가져오기' })
   @Get()
   async getNotices(): Promise<Notice[]> {
     return await this.noticeService.getNotices();
   }
 
+  @ApiOperation({ summary: '특정 게시글 가져오기' })
   @Get(':id')
   async getNotice(@Param('id') id: number): Promise<Notice> {
     return this.noticeService.getNotice({ id: id });
