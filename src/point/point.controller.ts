c1b25242d1ebea60f6fb8bc0761fff16180e3db4
5linesys <dhtjstlr777@gmail.com>
2021-09-30 19:36:44 +0900

    Add Point
    ----------Add-----------
    기존 메소드만 개발하고 라우팅 처리를 하지않아
    Get method 로 포인트 사용 기록을 불러올 수 있도록 했습니다.

--- src/point/point.controller.ts
+++ src/point/point.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Request,
+  Query,
+  BadRequestException,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { PointService } from './point.service';
+import { Point } from '@prisma/client';
+import { SkipTakeDto } from '../box/dto/skip-take.dto';
 
+@ApiTags('point')
 @Controller('point')
-export class PointController {}
+export class PointController {
+  constructor(private readonly pointService: PointService) {}
+
+  @ApiOperation({ summary: '사용자 포인트 사용기록 가져오기' })
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async getPoint(@Request() req, @Query() q: SkipTakeDto): Promise<Point[]> {
+    if (Math.abs(q.skip - q.take) > 100)
+      throw new BadRequestException(
+        '1회당 최대 요청 범위 초과',
+        'Bad request error',
+      );
+    return this.pointService.getPoint(req.user.userId);
+  }
+}
